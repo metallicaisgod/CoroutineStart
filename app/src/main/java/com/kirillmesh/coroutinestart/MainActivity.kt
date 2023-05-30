cf43c7bcbd54cb86a2b6c5221dd2fc18ff654fbd
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-05-30 10:44:21 +0300

    Job and Coroutine Builder

--- app/src/main/java/com/kirillmesh/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/kirillmesh/coroutinestart/MainActivity.kt
@@ -19,8 +19,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            val jobCity = lifecycleScope.launch {
+                val city = loadCity()
+                binding.tvLocation.text = city
+            }
+            val jobTemp = lifecycleScope.launch {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+            }
             lifecycleScope.launch {
-                loadData()
+                jobCity.join()
+                jobTemp.join()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
             }
         }
     }
@@ -30,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.isEnabled = false
         val city = loadCity()
         binding.tvLocation.text = city
-        val temp = loadTemperature(city)
+        val temp = loadTemperature()
         binding.tvTemperature.text = temp.toString()
         binding.progress.isVisible = false
         binding.buttonLoad.isEnabled = true
@@ -41,12 +54,7 @@ class MainActivity : AppCompatActivity() {
         return "Moscow"
     }
 
-    private suspend fun loadTemperature(city: String): Int {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
+    private suspend fun loadTemperature(): Int {
         delay(5000)
         return 17
     }
